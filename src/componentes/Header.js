59250020e24641161802e348c8f79a797f58ed83
RubenAlomar <rd_alomar@hotmail.com>
2023-05-02 21:29:14 -0300

    actualizacion 1

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,11 +3,9 @@ import React from 'react';
 const Header = () => {
     return(
         <nav className="navbar navbar-light">
-            <div>
-                <h1 className="navbar-brand font-weight-bold text-center" href="/">
-                    Noticias Actuales
-                </h1>
-            </div>              
+            <h1 className="navbar-brand font-weight-bold " href="/">
+                Noticias Actuales
+            </h1>             
         </nav>
     )
 };
